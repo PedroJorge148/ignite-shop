6eb094e41600f632c62f23cf55c2278a0f297229
Pedro Jorge <pedrojorge2011@gmail.com>
2023-02-03 14:38:01 -0300

    finished project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import type { AppProps } from 'next/app'
+import { Container, Header } from "../styles/pages/app";
+import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg';
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} width={logoImg.width} height={logoImg.height} alt="" priority />
+
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
